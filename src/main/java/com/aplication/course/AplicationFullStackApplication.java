a02c13646005147ad1e89ee80942fbb2adb32f8f
Lucas Rodrigues Monteiro <monteiro9961@gmail.com>
2022-02-22 02:49:33 -0300

    Projeto criado

---
+++ src/main/java/com/aplication/course/AplicationFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.aplication.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplication/course/AplicationFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplication.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
